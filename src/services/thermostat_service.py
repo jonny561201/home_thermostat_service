b33dac02ab1e135e13c50ea5beae0664c16eddc9
Jon Graf <jongraf1@gmail.com>
2021-02-25 20:15:19 -0600

    added service application to run the thermostat programming in a thread

--- src/services/thermostat_service.py
+++ src/services/thermostat_service.py
@@ -1,2 +1,20 @@
+from src.constants.home_automation import Automation
+from src.constants.hvac_state import HvacState
+from src.utilities import gpio_utils
+from src.utilities.gpio_utils import read_temperature_file
+from src.utilities.user_temp_utils import get_user_temperature
+
+
 def run_thermostat_program():
-    pass
+    temp_file = read_temperature_file()
+    celsius_temp = get_user_temperature(temp_file, False)
+    state = HvacState.get_instance()
+
+    if state.MODE == Automation.HVAC.MODE.COOLING and celsius_temp > state.DESIRED_TEMP:
+        gpio_utils.turn_on_hvac(Automation.HVAC.AIR_CONDITIONING)
+    elif state.MODE == Automation.HVAC.MODE.HEATING and celsius_temp < state.DESIRED_TEMP:
+        gpio_utils.turn_on_hvac(Automation.HVAC.FURNACE)
+    else:
+        gpio_utils.turn_off_hvac(Automation.HVAC.AIR_CONDITIONING)
+        gpio_utils.turn_off_hvac(Automation.HVAC.FURNACE)
+
---
+++ tests/unit/services/test_thermostat_service.py
@@ -0,0 +1,100 @@
+from mock import patch, ANY, call
+
+from src.constants.home_automation import Automation
+from src.constants.hvac_state import HvacState
+from src.services.thermostat_service import run_thermostat_program
+
+
+@patch('src.services.thermostat_service.gpio_utils')
+@patch('src.services.thermostat_service.get_user_temperature')
+@patch('src.services.thermostat_service.read_temperature_file')
+class TestHvac:
+    DESIRED_TEMP = 33.0
+    AC_TEMP = 35.0
+    HEAT_TEMP = 31.0
+
+    def setup_method(self):
+        self.HVAC = HvacState.get_instance()
+        self.HVAC.DESIRED_TEMP = self.DESIRED_TEMP
+        self.HVAC.MODE = Automation.HVAC.MODE.COOLING
+
+    def test_run_temperature_program__should_make_call_to_read_temperature_file(self, mock_temp, mock_convert, mock_gpio):
+        mock_convert.return_value = self.AC_TEMP
+        run_thermostat_program()
+        mock_temp.assert_called()
+
+    def test_run_temperature_program__should_call_get_user_temperature(self, mock_temp, mock_convert, mock_gpio):
+        mock_temp.return_value = self.AC_TEMP
+        mock_convert.return_value = self.AC_TEMP
+
+        run_thermostat_program()
+        mock_convert.assert_called()
+
+    def test_run_temperature_program__should_make_call_to_get_user_temperature_with_result_of_temp_file(self, mock_temp, mock_convert, mock_gpio):
+        mock_temp.return_value = self.AC_TEMP
+        mock_convert.return_value = self.AC_TEMP
+
+        run_thermostat_program()
+        mock_convert.assert_called_with(self.AC_TEMP, ANY)
+
+    def test_run_temperature_program__should_make_call_to_get_user_temperature_with_celsius(self, mock_temp, mock_convert, mock_gpio):
+        mock_convert.return_value = self.AC_TEMP
+
+        run_thermostat_program()
+        mock_convert.assert_called_with(ANY, False)
+
+    def test_run_temperature_program__should_not_call_ac_on_when_temp_below_desired(self, mock_temp, mock_convert, mock_gpio):
+        mock_convert.return_value = self.HEAT_TEMP
+
+        run_thermostat_program()
+        mock_gpio.turn_on_hvac.assert_not_called()
+
+    def test_run_temperature_program__should_turn_on_ac_when_temp_above_desired_and_mode_cooling(self, mock_temp, mock_convert, mock_gpio):
+        mock_convert.return_value = self.AC_TEMP
+
+        run_thermostat_program()
+        mock_gpio.turn_on_hvac.assert_called_with(Automation.HVAC.AIR_CONDITIONING)
+
+    def test_run_temperature_program__should_turn_on_furnace_when_temp_below_desired_and_mode_heating(self, mock_temp, mock_convert, mock_gpio):
+        self.HVAC.MODE = Automation.HVAC.MODE.HEATING
+        mock_convert.return_value = self.HEAT_TEMP
+
+        run_thermostat_program()
+        mock_gpio.turn_on_hvac.assert_called_with(Automation.HVAC.FURNACE)
+
+    def test_run_temperature_program__should_not_turn_on_furnace_when_temp_above_desired_and_mode_heating(self, mock_temp, mock_convert, mock_gpio):
+        self.HVAC.MODE = Automation.HVAC.MODE.HEATING
+        mock_convert.return_value = self.AC_TEMP
+
+        run_thermostat_program()
+        mock_gpio.turn_on_hvac.assert_not_called()
+
+    def test_run_temperature_program__should_turn_off_furnace_when_temp_above_desired_and_mode_heating(self, mock_temp, mock_convert, mock_gpio):
+        self.HVAC.MODE = Automation.HVAC.MODE.HEATING
+        mock_convert.return_value = self.AC_TEMP
+
+        run_thermostat_program()
+        mock_gpio.turn_off_hvac.assert_called_with(Automation.HVAC.FURNACE)
+
+    def test_run_temperature_program__should_turn_off_furnace_when_temp_equal_desired_and_mode_heating(self, mock_temp, mock_convert, mock_gpio):
+        self.HVAC.MODE = Automation.HVAC.MODE.HEATING
+        mock_convert.return_value = self.DESIRED_TEMP
+
+        run_thermostat_program()
+        mock_gpio.turn_off_hvac.assert_called_with(Automation.HVAC.FURNACE)
+
+    def test_run_temperature_program__should_turn_off_ac_and_furnace_when_temp_below_desired_and_mode_cooling(self, mock_temp, mock_convert, mock_gpio):
+        mock_convert.return_value = self.HEAT_TEMP
+        mock_call_1 = call(Automation.HVAC.AIR_CONDITIONING)
+        mock_call_2 = call(Automation.HVAC.FURNACE)
+
+        run_thermostat_program()
+        mock_gpio.turn_off_hvac.assert_has_calls([mock_call_1, mock_call_2])
+
+    def test_run_temperature_program__should_turn_off_ac_and_furnace_when_temp_equal_desired_and_mode_heating(self, mock_temp, mock_convert, mock_gpio):
+        mock_convert.return_value = self.DESIRED_TEMP
+        mock_call_1 = call(Automation.HVAC.AIR_CONDITIONING)
+        mock_call_2 = call(Automation.HVAC.FURNACE)
+
+        run_thermostat_program()
+        mock_gpio.turn_off_hvac.assert_has_calls([mock_call_1, mock_call_2])
