34daf80a2fd771d526f8f98e29c0d376d32b8b99
Jon Graf <jongraf1@gmail.com>
2021-02-25 19:11:03 -0600

    added utility method to create threads

---
+++ src/utilities/event_utils.py
@@ -0,0 +1,30 @@
+import logging
+from threading import Thread, Event
+
+from src.constants.home_automation import Automation
+
+
+class MyThread(Thread):
+    def __init__(self, event, sched_function, function_interval):
+        Thread.__init__(self)
+        self.stopped = event
+        self.function = sched_function
+        self.interval = function_interval
+
+    def run(self):
+        try:
+            while not self.stopped.wait(self.interval):
+                try:
+                    self.function()
+                except Exception as e:
+                    logging.error(f'Exception thrown invoking function')
+                    logging.error(e)
+        except Exception as e:
+            logging.error(f'Exception thrown during initial running of thread')
+            logging.error(e)
+
+
+# thread.stopped.set() will kill the process
+def create_thread(fn, delay=Automation.TIME.THIRTY_SECONDS):
+    stop_event = Event()
+    return MyThread(stop_event, fn, delay)
