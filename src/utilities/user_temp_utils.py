fdce984c401cda8f1b679146245e4f00864fe146
Jon Graf <jongraf1@gmail.com>
2021-02-25 19:47:45 -0600

    added utility methods to get the user temperature from an specific file format

---
+++ src/utilities/user_temp_utils.py
@@ -0,0 +1,27 @@
+import re
+
+
+from src.utilities.conversion_utils import convert_to_fahrenheit
+
+
+def get_user_temperature(temp_text, is_fahrenheit):
+    if temp_text[0][-3:] != 'YES':
+        return 0.0
+    temp_string = re.search('(t=\d*)', temp_text[1])
+    if temp_string is None:
+        return 0.0
+
+    celsius = _get_celsius_value(temp_string.group())
+    return _convert_to_correct_unit(celsius, is_fahrenheit)
+
+
+def _get_celsius_value(temp_row):
+    cleaned_text = temp_row.replace('t=', '')
+    temp_celsius = int(cleaned_text)
+    return round(temp_celsius / 1000, 2)
+
+
+def _convert_to_correct_unit(celsius, is_fahrenheit):
+    if is_fahrenheit:
+        return convert_to_fahrenheit(celsius)
+    return celsius
---
+++ tests/unit/utilities/test_user_temp_utils.py
@@ -0,0 +1,33 @@
+from src.utilities.user_temp_utils import get_user_temperature
+
+
+def test_get_user_temperature__should_return_temperature_in_celsius():
+    temp_text = ['72 01 4b 46 7f ff 0e 10 57 : crc=57 YES',
+                 '72 01 4b 46 7f ff 0e 10 57 t=23125']
+    actual = get_user_temperature(temp_text, False)
+
+    assert actual == 23.12
+
+
+def test_get_user_temperature__should_return_temperature_in_fahrenheit():
+    temp_text = ['72 01 4b 46 7f ff 0e 10 57 : crc=57 YES',
+                 '72 01 4b 46 7f ff 0e 10 57 t=12451']
+    actual = get_user_temperature(temp_text, True)
+
+    assert actual == 54.41
+
+
+def test_get_user_temperature__should_throw_conflict_when_error_reading_temp():
+    temp_text = ['72 01 4b 46 7f ff 0e 10 57 : crc=57 NOPE']
+    actual = get_user_temperature(temp_text, False)
+
+    assert actual == 0.0
+
+
+def test_get_user_temperature__should_throw_conflict_when_no_temp_text_found():
+    temp_text = ['72 01 4b 46 7f ff 0e 10 57 : crc=57 YES',
+                 '72 01 4b 46 7f ff 0e 10 57']
+    actual = get_user_temperature(temp_text, False)
+
+    assert actual == 0.0
+
