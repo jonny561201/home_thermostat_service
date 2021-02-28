f865b750a389e045e81b0f43e332b121a5636c35
Jon Graf <jongraf1@gmail.com>
2021-02-28 16:06:07 -0600

    updated to actually read the temperature file from the internal sensor

--- src/utilities/gpio_utils.py
+++ src/utilities/gpio_utils.py
@@ -19,17 +19,20 @@ FURNACE_PIN = 13
 
 
 def read_temperature_file():
-    return ['72 01 4b 46 7f ff 0e 10 57 : crc=57 YES',
-            '72 01 4b 46 7f ff 0e 10 57 t=23125']
+    file_name = "/sys/bus/w1/devices/28-000006dfa76c/w1_slave"
+    with open(file_name, 'r', encoding='utf-8') as file:
+        return file.read().split("\n")
+        return ['72 01 4b 46 7f ff 0e 10 57 : crc=57 YES',
+                '72 01 4b 46 7f ff 0e 10 57 t=23125']
 
 
 def turn_on_hvac(device):
-     # if device == Automation.HVAC.AIR_CONDITIONING:
-     #     GPIO.output(AC_PIN, GPIO.HIGH)
-     # else:
-     #     GPIO.output(FURNACE_PIN, GPIO.HIGH)
-     # GPIO.output(BLOWER_PIN, GPIO.HIGH)
-     pass
+    # if device == Automation.HVAC.AIR_CONDITIONING:
+    #     GPIO.output(AC_PIN, GPIO.HIGH)
+    # else:
+    #     GPIO.output(FURNACE_PIN, GPIO.HIGH)
+    # GPIO.output(BLOWER_PIN, GPIO.HIGH)
+    pass
 
 
 def turn_off_hvac(device):
