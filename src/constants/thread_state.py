c7c0ed37e64cab044fbb0b1a57db7a5719a142c7
Jon Graf <jongraf1@gmail.com>
2021-03-13 14:50:56 -0600

    updated the hvac state object to store the start and stop temps

--- src/constants/thread_state.py
+++ src/constants/thread_state.py
@@ -10,10 +10,14 @@ class HvacState(ThreadState):
     TRIGGERED = False
     START_TIME = None
     STOP_TIME = None
+    START_TEMP = None
+    STOP_TEMP = None
     DAYS = None
 
-    def __init__(self, task_id: str, days: str, start: datetime.time, stop: datetime.time, ):
+    def __init__(self, task_id: str, days: str, start: datetime.time, stop: datetime.time, start_temp: int, stop_temp: int):
         self.THREAD_ID = task_id
         self.DAYS = days
+        self.START_TEMP = start_temp
+        self.STOP_TEMP = stop_temp
         self.START_TIME = datetime.datetime.combine(datetime.date.today(), start).time()
         self.STOP_TIME = datetime.datetime.combine(datetime.date.today(), stop).time()
