fd6e877a37a33d603c2d74e80cec2a2b0140f6b0
Jon Graf <jongraf1@gmail.com>
2021-03-14 18:01:08 -0500

    refactored the run thermostat program to more logically break apart for automated thermostat program

--- src/services/thermostat_service.py
+++ src/services/thermostat_service.py
@@ -15,9 +15,20 @@ def run_thermostat_program(event_state):
     celsius_temp = get_user_temperature(temp_file, False)
     state = get_desired_temp()
 
-    if state['mode'] == Automation.HVAC.MODE.COOLING and celsius_temp > state['desiredTemp']:
+    if state['mode'] is None:
+        __run_automated_hvac(celsius_temp, state)
+    else:
+        __run_manual_hvac(celsius_temp, state['mode'], state['desiredTemp'])
+
+
+def __run_automated_hvac(celsius_temp, state):
+    print('do stuff here')
+
+
+def __run_manual_hvac(celsius_temp, mode, desired_temp):
+    if mode == Automation.HVAC.MODE.COOLING and celsius_temp > desired_temp:
         gpio_utils.turn_on_hvac(Automation.HVAC.AIR_CONDITIONING)
-    elif state['mode'] == Automation.HVAC.MODE.HEATING and celsius_temp < state['desiredTemp']:
+    elif mode == Automation.HVAC.MODE.HEATING and celsius_temp < desired_temp:
         gpio_utils.turn_on_hvac(Automation.HVAC.FURNACE)
     else:
         gpio_utils.turn_off_hvac(Automation.HVAC.AIR_CONDITIONING)
