0bd61833af5905c89fc8649883e8209ae89c8119
Jon Graf <jongraf1@gmail.com>
2021-03-16 23:03:43 -0500

    added new endpoint to hit the hub and get the users weather data

--- src/utilities/api_utils.py
+++ src/utilities/api_utils.py
@@ -10,3 +10,8 @@ def get_hvac_tasks_by_user(user_id):
         return response.json()
     except Exception:
         return None
+
+
+def get_weather_data_by_user(user_id):
+    base_url = Settings.get_instance().hub_base_url
+    requests.get(f'{base_url}/temperature/{user_id}')
--- tests/unit/utilities/test_api_utils.py
+++ tests/unit/utilities/test_api_utils.py
@@ -1,11 +1,11 @@
 import json
 import os
-from datetime import time
 
 from mock import patch
 from requests import Response
+
 from src.constants.settings_state import Settings
-from src.utilities.api_utils import get_hvac_tasks_by_user
+from src.utilities.api_utils import get_hvac_tasks_by_user, get_weather_data_by_user
 
 
 @patch('src.utilities.api_utils.requests')
@@ -44,6 +44,11 @@ class TestLightApiRequests:
         actual = get_hvac_tasks_by_user(self.USER_ID)
 
         assert actual is None
+
+    def test_get_weather_data_by_user__should_make_rest_call_using_url(self, mock_requests):
+        get_weather_data_by_user(self.USER_ID)
+
+        mock_requests.get.assert_called_with(f'{self.BASE_URL}/temperature/{self.USER_ID}')
 
     @staticmethod
     def __create_response(status=200, data=None):
